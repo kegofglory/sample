0173432fe22787218e399534ca1775d0c67dd858
kegofglory <akki611dayo@gmail.com>
2019-09-22 00:41:37 +0900

    slim, bootstrap追加

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module Sample
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators.template_engine = :slim
+
+    config.generators do |g|
+      g.helper false
+      g.assets false
+    end
   end
 end
